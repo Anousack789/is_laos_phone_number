cf645b74da0193dbddf659d4a3f70929dc65c5fd
Anousack Phounsilakone <64178877+Anousack789@users.noreply.github.com>
2024-09-29 13:21:18 +0700

    chore: export isLaosPhoneNumber as default and move isNumber to default function

--- src/functions.ts
+++ src/functions.ts
@@ -1,7 +1,3 @@
-export function isNumber(input: string) {
-  const num = Number(input);
-  return !isNaN(Number(input)) && num >= 1;
-}
 const prefixFor8digit = ["+85620", "85620", "20", "020"];
 
 const prefixFor7digit = [
@@ -15,13 +11,16 @@ const prefixFor7digit = [
   "030",
 ];
 
-export function isLaosPhoneNumber(phoneNumber: string) {
+export default function isLaosPhoneNumber(phoneNumber: string) {
   if (phoneNumber.length > 14) {
     return false;
   }
 
   // verify input number is number string
-  if (!isNumber(phoneNumber)) {
+
+  const num = Number(phoneNumber);
+
+  if (isNaN(num) || num < 1) {
     return false;
   }
 
@@ -30,12 +29,16 @@ export function isLaosPhoneNumber(phoneNumber: string) {
   }
 
   let result = prefixFor8digit.some(
-    (prefix) => phoneNumber.startsWith(prefix) && phoneNumber.replace(prefix, "").length === 8
+    (prefix) =>
+      phoneNumber.startsWith(prefix) &&
+      phoneNumber.replace(prefix, "").length === 8
   );
 
   if (!result) {
     result = prefixFor7digit.some(
-      (prefix) => phoneNumber.startsWith(prefix) && phoneNumber.replace(prefix, "").length === 7
+      (prefix) =>
+        phoneNumber.startsWith(prefix) &&
+        phoneNumber.replace(prefix, "").length === 7
     );
   }
 
